31d8cba1c24e2bca0cbed633d33d6857d246f5fd
cchuq001 <43690384+cchuq001@users.noreply.github.com>
2025-05-02 01:34:16 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("This is code to share off my Linux and Windows for Github! Vamonos!!!!");
-        System.out.println("Lets goooo!");
 
         }
     }
